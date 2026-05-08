1f7ea6afd44ad8296211085dc3ded96cc35e3902
subhadeepsarkar <subhadeepsarkarybs@yahoo.com>
2026-05-08 22:53:50 +0000

    Deploying to gh-pages from @ subhadeepsarkar/subhadeepsarkar.github.io@311cc41e2168dd66926577e8eebef7063ac085ba 🚀

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,11 +1,42 @@
 $(document).ready(function() {
-    $('a.abstract').click(function() {
-        $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
-        $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
+    // Hide all abstract/bibtex blocks on load
+    $(".abstract.hidden, .bibtex.hidden").hide();
+
+    function slideToggleAbstract($container) {
+        var $abstract = $container.find(".abstract.hidden");
+        var $bibtex   = $container.find(".bibtex.hidden");
+        if ($abstract.is(':visible')) {
+            $abstract.slideUp(300).removeClass('open');
+        } else {
+            $bibtex.slideUp(300).removeClass('open');
+            $abstract.slideDown(300).addClass('open');
+        }
+    }
+
+    function slideToggleBibtex($container) {
+        var $abstract = $container.find(".abstract.hidden");
+        var $bibtex   = $container.find(".bibtex.hidden");
+        if ($bibtex.is(':visible')) {
+            $bibtex.slideUp(300).removeClass('open');
+        } else {
+            $abstract.slideUp(300).removeClass('open');
+            $bibtex.slideDown(300).addClass('open');
+        }
+    }
+
+$('.abstract-toggle-block').click(function(e) {
+        if ($(e.target).closest('a').length) return;
+        slideToggleAbstract($(this).closest('.col-sm-8'));
     });
     $('a.bibtex').click(function() {
-        $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
-        $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
+        slideToggleBibtex($(this).parent().parent());
     });
     $('a').removeClass('waves-effect waves-light');
+
+    $('.pub-icon[title]').tooltip({
+        delay: { show: 100, hide: 50 },
+        placement: 'top',
+        container: 'body',
+        trigger: 'hover'
+    });
 });
